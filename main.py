29ea10f7acedca171d120e58fea0b744ebde8629
Airik Warren <AirikWarren@gmail.com>
2021-03-04 17:04:43 -0800

    Getting ready for presentation night

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 import os 
 import shutil
 
-import jinja2
 from jinja2.environment import Environment
 from jinja2.loaders import FileSystemLoader
 
-try:
+try: # Output directory 
     os.mkdir('./output')
     os.mkdir('./output/style')
 except FileExistsError:
@@ -15,17 +14,18 @@ except FileExistsError:
 
 def generate_title_html_dict(env : Environment) -> dict:
     ''' for a file name.html, dictionary k,v pair would be "name":"name.html" '''
-    nav_links = {x[:-5]:f'./{x}' for x in env.list_templates()}
-    nav_links['Home'] = "./index.html"
+    nav_links = {x[:-5]:f'./{x}' for x in env.list_templates()} # What the fuck? 
+    nav_links['Home'] = "./index.html" # Seriously though it's just removing the file extenstion
     nav_links.pop('index')
     nav_links.pop('base')
     return nav_links
 
 if __name__ == '__main__':
-    env = jinja2.Environment(loader=FileSystemLoader('./templates/html_templates'))
+    html_template_path = './templates/html_templates' 
+    env = Environment(loader=FileSystemLoader('./templates/html_templates'))
     filenames = [x for x in env.list_templates() if "base.html" not in x]
     nav_links = generate_title_html_dict(env)
-
+    
     for x in os.listdir('./templates/css_templates'):
         shutil.copyfile(f'./templates/css_templates/{x}', f'./output/style/{x}')
 
